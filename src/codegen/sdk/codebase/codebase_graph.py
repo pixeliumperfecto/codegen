27a69a8f2dd7aaa532573cba093b491685eb1127
eacodegen <eagarwal@codegen.com>
2025-02-05 14:39:23 -0800

    Ignore folder (#317)
    Co-authored-by: bagel897 <ellenagarwal897@gmail.com>

--- src/codegen/sdk/codebase/codebase_graph.py
+++ src/codegen/sdk/codebase/codebase_graph.py
@@ -51,7 +51,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-GLOBAL_FILE_IGNORE_LIST = [".git/*", ".yarn/releases/*", ".*/tests/static/chunk-.*.js"]
+GLOBAL_FILE_IGNORE_LIST = [".git/*", ".yarn/releases/*", ".*/tests/static/chunk-.*.js", ".*/ace/.*.js"]
 
 
 @unique
