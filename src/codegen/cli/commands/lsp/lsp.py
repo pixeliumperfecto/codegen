13765da23664aeba352afcfb258cee251810c585
Ellen Agarwal <ellenagarwal897@gmail.com>
2025-02-13 15:52:40 -0800

    fix: Make lsp an optional dependency (#492)

--- src/codegen/cli/commands/lsp/lsp.py
+++ src/codegen/cli/commands/lsp/lsp.py
@@ -2,10 +2,15 @@ import logging
 
 import click
 
-from codegen.extensions.lsp.lsp import server
+logger = logging.getLogger(__name__)
 
 
 @click.command(name="lsp")
 def lsp_command():
+    try:
+        from codegen.extensions.lsp.lsp import server
+    except ImportError:
+        logger.exception("LSP is not installed. Please install it with `uv tool install codegen[lsp]`")
+        return
     logging.basicConfig(level=logging.INFO)
     server.start_io()
