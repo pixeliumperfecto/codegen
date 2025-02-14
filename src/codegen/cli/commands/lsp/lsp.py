95e5ab0d8824511e9f33cc7e5cc875783c42f047
Ellen Agarwal <ellenagarwal897@gmail.com>
2025-02-13 16:14:54 -0800

    fix: import bug (#493)
    # Motivation
    
    <!-- Why is this change necessary? -->
    
    # Content
    
    <!-- Please include a summary of the change -->
    
    # Testing
    
    <!-- How was the change tested? -->
    
    # Please check the following before marking your PR as ready for review
    
    - [ ] I have added tests for my changes
    - [ ] I have updated the documentation or added new documentation as
    needed

--- src/codegen/cli/commands/lsp/lsp.py
+++ src/codegen/cli/commands/lsp/lsp.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 def lsp_command():
     try:
         from codegen.extensions.lsp.lsp import server
-    except ImportError:
-        logger.exception("LSP is not installed. Please install it with `uv tool install codegen[lsp]`")
+    except (ImportError, ModuleNotFoundError):
+        logger.exception("LSP is not installed. Please install it with `uv tool install codegen[lsp] --prerelease=allow`")
         return
     logging.basicConfig(level=logging.INFO)
     server.start_io()
